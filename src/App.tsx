52feb49e4f1645982413b40a52a626be3eb47cfb
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-03-01 11:24:39 +0330

    add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
+import { Button, Stack } from 'react-bootstrap';
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Stack direction="horizontal" gap={2}>
+                <Button as="a" variant="primary">
+                    Button as link
+                </Button>
+                <Button as="a" variant="success">
+                    Button as link
+                </Button>
+            </Stack>
+        </div>
+    );
 }
 
 export default App;
