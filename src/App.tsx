2817ba197c72dadab19edafaba3c631755cf40ec
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-03-02 19:57:40 +0330

    complete under slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,154 @@
-import { Button, Stack } from 'react-bootstrap';
-import logo from './logo.svg';
+import NavbarComponent from "./shared/Navbar";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper";
+import firstImage from "./assets/img/image1.png";
+import { InputGroup } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-    return (
-        <div className="App">
-            <Stack direction="horizontal" gap={2}>
-                <Button as="a" variant="primary">
-                    Button as link
-                </Button>
-                <Button as="a" variant="success">
-                    Button as link
-                </Button>
-            </Stack>
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>
+          <div className="d-flex flex-row-reverse align-items-center">
+            <div>
+              <img src={firstImage} alt="" />
+            </div>
+            <div className="flex-column">
+              <h3>Fire Up Your Business With Us</h3>
+              <p>
+                Are you one of the thousands of Iphone owners who has no idea.{" "}
+              </p>
+
+              <InputGroup className="mb-3 mt-3">
+                <InputGroup.Text
+                  style={{
+                    background: "transparent",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+                <Form.Control
+                  aria-label="Last name"
+                  placeholder="Find services"
+                />
+                <InputGroup.Text>Search</InputGroup.Text>
+              </InputGroup>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="d-flex flex-row-reverse align-items-center">
+            <div>
+              <img src={firstImage} alt="" />
+            </div>
+            <div className="flex-column">
+              <h3>Fire Up Your Business With Us</h3>
+              <p>
+                Are you one of the thousands of Iphone owners who has no idea.{" "}
+              </p>
+
+              <InputGroup className="mb-3 mt-3">
+                <InputGroup.Text
+                  style={{
+                    background: "transparent",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+                <Form.Control
+                  aria-label="Last name"
+                  placeholder="Find services"
+                />
+                <InputGroup.Text>Search</InputGroup.Text>
+              </InputGroup>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="d-flex flex-row-reverse align-items-center">
+            <div>
+              <img src={firstImage} alt="" />
+            </div>
+            <div className="flex-column">
+              <h3>Fire Up Your Business With Us</h3>
+              <p>
+                Are you one of the thousands of Iphone owners who has no idea.{" "}
+              </p>
+
+              <InputGroup className="mb-3 mt-3">
+                <InputGroup.Text
+                  style={{
+                    background: "transparent",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+                <Form.Control
+                  aria-label="Last name"
+                  placeholder="Find services"
+                />
+                <InputGroup.Text>Search</InputGroup.Text>
+              </InputGroup>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="d-flex flex-row-reverse align-items-center">
+            <div>
+              <img src={firstImage} alt="" />
+            </div>
+            <div className="flex-column">
+              <h3>Fire Up Your Business With Us</h3>
+              <p>
+                Are you one of the thousands of Iphone owners who has no idea.{" "}
+              </p>
+
+              <InputGroup className="mb-3 mt-3">
+                <InputGroup.Text
+                  style={{
+                    background: "transparent",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+                <Form.Control
+                  aria-label="Last name"
+                  placeholder="Find services"
+                />
+                <InputGroup.Text>Search</InputGroup.Text>
+              </InputGroup>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+      <div className="row">
+        <div className="container">
+          <div className="col-6">
+            <p>
+              n publishing and graphic design, Lorem ipsum is a placeholder text
+              commonly used to demonstrate the visual form of a document or a
+              typeface without relying on meaningful content. Lorem ipsum may be
+              used as a placeholder before final copy is available.
+            </p>
+          </div>
+          <div className="col-6">
+          
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/shared/Navbar.tsx
@@ -0,0 +1,36 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+
+function NavbarComponent() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto align-items-center">
+            <div>
+              <InputGroup className="mb-3 mt-3">
+                <Form.Control aria-label="Last name" />
+              </InputGroup>
+            </div>
+
+            <Nav.Link href="#home">Business</Nav.Link>
+            <Nav.Link href="#home">Explore</Nav.Link>
+            <Nav.Link href="#link">Log in</Nav.Link>
+
+            <Button variant="outline-success" size="sm">
+              Sign Up
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
